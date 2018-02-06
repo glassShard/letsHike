8cb688a24444dde8253ad2006c1e60ed35277329
glassShard <info@uvegszilank.hu>
2018-02-06 16:47:52 +0100

    create user module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,23 +3,15 @@ import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {PageNotFoundComponent} from './core/page-not-found/page-not-found.component';
 import {UsersComponent} from './users/users.component';
-import {ProfileComponent} from './user/profile/profile.component';
-import {ProfileEditComponent} from './user/profile-edit/profile-edit.component';
-import {RegistrationComponent} from './user/registration/registration.component';
-import {LoggedInGuard} from './shared/logged-in.guard';
 
 const routes: Routes = [
   {path: 'kezdolap', component: HomeComponent},
+  {path: 'tobbiek', component: UsersComponent},
 
   {path: 'turak', loadChildren: 'app/event/event.module#EventModule'},
   {path: 'cuccok', loadChildren: 'app/item/item.module#ItemModule'},
+  {path: 'user', loadChildren: 'app/user/user.module#UserModule'},
 
-  {path: 'tobbiek', component: UsersComponent},
-  {path: 'user', children: [
-      {path: '', component: ProfileComponent, canActivate: [LoggedInGuard]},
-      {path: 'edit', component: ProfileEditComponent, canActivate: [LoggedInGuard]},
-      {path: 'registration', component: RegistrationComponent}
-    ]},
   {path: '', redirectTo: '/kezdolap', pathMatch: 'full'},
   {path: '**', component: PageNotFoundComponent}
 ];
@@ -32,9 +24,6 @@ const routes: Routes = [
 export class AppRoutingModule {
   static routableComponents = [
     HomeComponent,
-    RegistrationComponent,
-    ProfileComponent,
-    ProfileEditComponent,
     UsersComponent,
     PageNotFoundComponent
   ];
